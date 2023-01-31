7973e68b440c5ed2a351bcacb975c7f984c63e1d
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-01-31 10:40:47 -0800

    record title of page as well

--- obsidian-cliplet.js
+++ obsidian-cliplet.js
@@ -3,8 +3,9 @@
         const note_name = "web_clippings";
         const append = "true";
         const url = window.location.href;
+        const title = document.title;
         const text = window.getSelection ? window.getSelection().toString() : "";
-        const content = encodeURIComponent(`${url}\n\n${text}\n\n`);
+        const content = encodeURIComponent(`${title}\n${url}\n\n${text}\n\n\n`);
         const final_url = `obsidian://new?name=${note_name}&append=${append}&content=${content}`;
         window.location.href = final_url;
     }
